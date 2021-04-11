c6fe0b105a457247667c03271ab75f7e9a86ad7e
Alexandre G. Soares <alexandregsoares@gmail.com>
2021-04-11 12:27:57 +0200

    More debug information

--- touch-debugger.py
+++ touch-debugger.py
@@ -4,14 +4,12 @@ import random
 # Constants
 RADIUS = 10
 
-#counter = 0
-
 def random_color():
     colors = ["red", "blue", "pink", "cyan", "black", "aqua", "green", "yellow", "purple", "orange", "brown", "grey"]
     color = random.randrange(0,len(colors))
     #print(color)
     #print(colors[color])
-    return colors[color] 
+    return colors[color]
 
 def exit_screen():
     #root.attributes("-fullscreen", False)
@@ -31,6 +29,8 @@ def create_circle(xr, yr, radius):
     canvas.create_oval(x0, y0, x1, y1, fill=color)
     XData.set(xr)
     YData.set(yr)
+    colorData.set(color)
+    labelColor.config(fg=color)
 
 root = Tk()
 root.attributes("-fullscreen", True)
@@ -43,6 +43,8 @@ XData = IntVar()
 XData.set(0)
 YData = IntVar()
 YData.set(0)
+colorData = StringVar()
+colorData.set("-")
 
 root.update()
 # Drawable canvas
@@ -67,7 +69,8 @@ labelYStatic = Label(groupboxData, text='Y:')
 labelYStatic.grid(row=1, column=0)
 labelY = Label(groupboxData, padx=5, pady=5, textvariable=YData)
 labelY.grid(row=1, column=1)
-labelColor = Label
+labelColor = Label(groupboxData, padx=5, pady=5, textvariable=colorData, font=" Time 12 bold")
+labelColor.grid(row=2, column=0, columnspan=2)
 
 
 buttonExit = Button(root, padx=10, pady=5, width=10, text="Exit", command=exit_screen)
